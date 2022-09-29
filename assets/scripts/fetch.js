e68b15263ec8a839c81475786a125dcc9b36f93a
csara <csara715@gmail.com>
2022-09-28 21:45:14 -0600

    updated img src in index.html with upcoming posters

--- assets/scripts/fetch.js
+++ assets/scripts/fetch.js
@@ -1,21 +1,21 @@
-var imgEl = document.querySelectorAll("img");
+// var imgEl = document.querySelectorAll("img");
 
-function getUpcoming() {
-  const tmdbURL = `https://api.themoviedb.org/3/movie/upcoming?api_key=${tmdbKey}`;
-  const imgBaseURL = "https://image.tmdb.org/t/p/";
-  const fileSize = "w300";
-  fetch(tmdbURL)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-      for (var i = 0; i < 10; i++) {
-        imgEl[i].setAttribute("src", imgBaseURL + fileSize + data.results[i].poster_path);
-        }
-    });
-}
- getUpcoming();
+// function getUpcoming() {
+//   const tmdbURL = `https://api.themoviedb.org/3/movie/upcoming?api_key=${tmdbKey}`;
+//   const imgBaseURL = "https://image.tmdb.org/t/p/";
+//   const fileSize = "w300";
+//   fetch(tmdbURL)
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//       for (var i = 0; i < 10; i++) {
+//         imgEl[i].setAttribute("src", imgBaseURL + fileSize + data.results[i].poster_path);
+//         }
+//     });
+// }
+//  getUpcoming();
 // getSearchResults({
 // Next 2 lines are for searching by ID
 // searchBar: "tt2380307",
